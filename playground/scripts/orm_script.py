a95a73f21b56c55804714139cf69b03d2a5c71a2
Cristian Dumbravanu <Maincristian2233@gmail.com>
2025-07-03 23:33:02 +0100

    Last update

--- playground/scripts/orm_script.py
+++ playground/scripts/orm_script.py
@@ -1,8 +1,18 @@
-from playground.models import Restaurant, Rating, Sale, Staff
+from playground.models import Restaurant, Rating, Sale, Staff, StaffRestaurant
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import connection
 from django.db.models.functions import Lower
 
 def run():
+    staff, created = Staff.objects.get_or_create(name = "John Wich")
+    restaurant = Restaurant.objects.first()
+    restaurant2 = Restaurant.objects.last()
     
+    StaffRestaurant.objects.create(
+        staff = staff, restaurant = restaurant, salary = 28000
+    )
+
+    StaffRestaurant.objects.create(
+        staff = staff, restaurant = restaurant2, salary = 30000
+    )
