e3fecbdcf6c13130dcb4736038461a028282112b
Cristian Dumbravanu <Maincristian2233@gmail.com>
2025-07-04 10:58:15 +0100

    Aggregate is Done

--- playground/scripts/orm_script.py
+++ playground/scripts/orm_script.py
@@ -2,11 +2,12 @@ from playground.models import Restaurant, Rating, Sale, Staff, StaffRestaurant
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.db import connection
-from django.db.models.functions import Lower
+from django.db.models.functions import Lower, Length
 import random
 from django.db.models.functions import Upper
+from django.db.models import Count, Avg, Min, Max
 
 
 def run():
-    restaurants = Restaurant.objects.values_list('name','dataOpened')
-    print(restaurants)
+    # Fetch all restaurants and want to get the number of characters in the name of the restaurant. e.g. 'xyz' == 3
+    
