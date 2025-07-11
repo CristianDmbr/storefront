cef0e57969ce426971170938b1860bb16a033c67
Cristian Dumbravanu <Maincristian2233@gmail.com>
2025-07-12 00:36:33 +0100

    subsequence

--- playground/scripts/orm_script.py
+++ playground/scripts/orm_script.py
@@ -5,31 +5,14 @@ from django.db import connection
 from django.db.models.functions import Lower, Length
 import random
 from django.db.models.functions import Upper, Length, Concat, Coalesce
-from django.db.models import Count, Avg, Min, Max, CharField, Value, Case, When, Sum, F, Q, OuterRef, Subquery 
+from django.db.models import Count, Avg, Min, Max, CharField, Value, Case, When, Sum, F, Q, OuterRef, Subquery, Exists
 
 
 def run():
-    # Check average rating > 3.5 and restaurants have more than 1 rating
-
-    restaurants = Restaurant.objects.annotate(averageRating = Avg('ratings__rating'), nRatings = Count('ratings__pk'))
-
-    restaurants = restaurants.annotate(
-        highlyRated = Case(
-            When(averageRating__gt = 3.5, nRatings__gt = 1, then = True),
-            default = False
-        )
+    # Filter restaurants that have sales with income of more than 85.
+    restaurants = Restaurant.objects.filter(
+        Exists(Sale.objects.filter(restaurant = OuterRef('pk'), income__gt = 90))
     )
 
-    restaurants = restaurants.annotate(
-        ratingBucket = Case(
-            When(avg__gt = 3.5, then = Value('Highly Rated')),
-            When(avg_range = (2.5, 3.5), then = Value('Average Rating')),
-            When(avg_lt = 2.5, then = Value('Bad Rating')),
-            default = False
-        )
-    )
-
-    # Assign continets to each restaurant
-
-    print(restaurants.filter(highlyRated = False))
-    print(restaurants.filter(ratingBucket = 'Highly Rated'))
+    print(restaurants)
+    
